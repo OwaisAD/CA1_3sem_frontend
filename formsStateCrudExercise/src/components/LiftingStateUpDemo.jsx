88276020ca6e4001dd72e365a2a15f12ac97a965
Owais Dashti <owaisdashti@outlook.com>
2022-10-27 08:57:23 +0200

    fix

--- formsStateCrudExercise/src/components/LiftingStateUpDemo.jsx
+++ formsStateCrudExercise/src/components/LiftingStateUpDemo.jsx
@@ -23,6 +23,7 @@ const LiftingStateUpDemo = () => {
 
     const [persons, setPersons] = useState(initialValue)
     const [newPerson, setNewPerson] = useState({id: "", name: ""})
+    const [editMode, setEditMode] = useState(false)
 
     // add person method for the input component
     const addPerson = person => {
@@ -32,6 +33,7 @@ const LiftingStateUpDemo = () => {
         } else { // if id already exists we just want to change it
             let personToEdit = persons.find(p => p.id === person.id)
             personToEdit.name = person.name
+            setEditMode(false)
         }
         setPersons([...persons])
         setNewPerson({id:"", name: ""})
@@ -44,12 +46,11 @@ const LiftingStateUpDemo = () => {
 
 
     const editPerson = id => {
+        setEditMode(true)
         console.log(id)
-
         // get person and display his name in the input field
         let personToEdit = persons.find(p => p.id === id)
         setNewPerson(personToEdit)
-        
     }
 
   return (
@@ -58,7 +59,7 @@ const LiftingStateUpDemo = () => {
         <p>Total persons: {persons.length}</p>
         <div style={{display: "flex"}}>
         <AllPersonsComponent persons={persons} deletePerson={deletePerson} editPerson={editPerson}/>
-        <InputPersonComponent addPerson={addPerson} nextPerson={newPerson}/>
+        <InputPersonComponent addPerson={addPerson} nextPerson={newPerson} editMode={editMode}/>
         </div>        
     </div>
   )
@@ -91,6 +92,7 @@ const InputPersonComponent = (props) => {
                 <input type="text" placeholder='Person name' value={person.name} onChange={onChange} required/>
                 <input type="submit" value="Save" onClick={savePerson}/>
             </form>
+            {props.editMode && <p>Currently in edit mode</p>}
         </div>
     )
 }
@@ -101,8 +103,9 @@ const AllPersonsComponent = ({persons,deletePerson, editPerson}) => {
             <h2>All Persons</h2>
             <ul>
                 {persons.map(person => {
-                    return <li key={person.id}>{person.name} (id: {person.id}) (<FaPen style={{color:"orange", cursor: "pointer"}} onClick={() => editPerson(person.id)}/> / <FaTimes style={{color:"red", cursor: "pointer"}} onClick={() => deletePerson(person.id)}/>)</li>
+                    return <li key={person.id}>{person.name} (id: {person.id.slice(2,7)}) (<FaPen style={{color:"orange", cursor: "pointer"}} onClick={() => editPerson(person.id)}/> / <FaTimes style={{color:"red", cursor: "pointer"}} onClick={() => deletePerson(person.id)}/>)</li>
                 })}
+                {!persons.length && <p>Nothing to see here :)</p>}
             </ul>
         </div>
     )
